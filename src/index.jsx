783c4fb8d4e248a79d1c523aecbb04c401dd768e
Pedro <pedro7109.j@gmail.com>
2022-11-21 12:56:19 -0300

    Model Dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import  ReactDOM  from "react";
 import App from "./main/App";
 
-ReactDOM.render(<app/>, document.getElementById('app'))
+ReactDOM.render(<App/>, document.getElementById('app'))
