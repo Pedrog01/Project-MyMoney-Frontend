3a79ed2284dc0219e230b672a4715b0f348a891d
Pedro <pedro7109.j@gmail.com>
2022-11-23 09:42:30 -0300

    component TabContent

--- src/common/tab/tabHeader.jsx
+++ src/common/tab/tabHeader.jsx
@@ -10,7 +10,7 @@ class TabHeader extends Component{
     render(){
         const selected = this.props.tab.selected === this.props.target
         return(
-            <li>
+            <li className={selected ? 'active' : ''}>
                 <a href ='javascript:;'
                 data-toggle='tab'
                 onClick={() => this.props.selectTab(this.props.target)}
