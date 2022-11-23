f5b883114c90a9b8ba9a3270acc03e6562d04ceb
Pedro <pedro7109.j@gmail.com>
2022-11-23 08:27:38 -0300

    BillingCycle Component: Refactoring to Class

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Router, Route, Redirect, hashHistory } from "react-router";
 
-import dashboard2 from '../dashboard2/dashboard2'
+import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={dashboard2} />
+        <Route path='/' component={Dashboard} />
         <Route path='/billingCycles' component={BillingCycle} />
         <Redirect from='*' to='/' />
     </Router>
