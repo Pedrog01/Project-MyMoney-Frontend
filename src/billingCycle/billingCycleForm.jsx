74526ec227c1bb1041b1f7120467d55df1848e73
Pedro <pedro7109.j@gmail.com>
2022-11-24 09:39:27 -0300

    fixing bugs and displaying success and error messages

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -3,7 +3,7 @@ import {reduxForm, Field} from 'redux-form'
 
 class BillingCycleForm extends Component {
     render(){
-        const { handleSubmit } = this.props
+        const { handleSubmit } = this.props 
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
