6e43519fb422e6d4f1bdbe6839122d4d61409946
Pedro <pedro7109.j@gmail.com>
2022-11-25 08:59:19 -0300

    component input to CrediList

--- src/billingCycle/creditList.jsx
+++ src/billingCycle/creditList.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import { Field } from "redux-form";
 import Grid from "../common/layout/grid";
+import Input from "../common/form/input";
 
 class CreditList extends Component{
 
     renderRows() {
         return(
             <tr>
-                <td><Field name='credits[0].name' component='input' /> </td>
-                <td><Field name='credits[0].value' component='input' /> </td>
+                <td><Field name='credits[0].name' component={Input}
+                placeholder='Informe seu Nome' readOnly={this.props.readOnly} /> </td>
+                <td><Field name='credits[0].value' component={Input}
+                placeholder='Informe o Valor' readOnly={this.props.readOnly}  /> </td>
                 <td></td>
             </tr>
         )
@@ -28,7 +31,7 @@ class CreditList extends Component{
                             </tr>
                         </thead>
                         <tbody>
-                            {this.renderRows}
+                            {this.renderRows()}
                         </tbody>
                    </table>
             </fieldset>
