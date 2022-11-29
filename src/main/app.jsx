75394d86402add91553a17eb0316e0e9a0843c93
Pedro Gabriel <pedro7109.j@gmail.com>
2022-11-29 10:55:43 -0300

    Creating authOrApp component

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,20 +1,18 @@
-import '../common/template/dependecies'
 import React from "react";
 
 import Header from '../common/template/header';
 import SideBar from '../common/template/sideBar';
 import Footer from '../common/template/footer';
-import Routes from './routes'
-import Message from '../common/msg/message';
+import Messages from '../common/msg/messages';
 
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
         <div className='content-wrapper'>
-            <Routes/>
+            {props.children}
         </div>
-        <Footer/>
-        <Message />
+        <Footer />
+        <Messages />
     </div>
 )
